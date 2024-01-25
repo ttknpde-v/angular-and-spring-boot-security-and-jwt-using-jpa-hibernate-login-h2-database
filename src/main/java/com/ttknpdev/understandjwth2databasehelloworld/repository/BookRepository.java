034ab97c7e91ac60da2c51e83b7f5e55918e2f73
Thitikorn <97865082+ttknpde-v@users.noreply.github.com>
2024-01-25 13:47:13 +0700

    Delete src/main directory

--- src/main/java/com/ttknpdev/understandjwth2databasehelloworld/repository/BookRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ttknpdev.understandjwth2databasehelloworld.repository;
-
-import com.ttknpdev.understandjwth2databasehelloworld.entities.Book;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookRepository extends CrudRepository<Book,String> { }
--- src/main/java/com/ttknpdev/understandjwth2databasehelloworld/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ttknpdev.understandjwth2databasehelloworld.repository;
-
-import com.ttknpdev.understandjwth2databasehelloworld.entities.User;
-import org.springframework.data.repository.CrudRepository;
-
-// use passed @service So it do not need to use @Repository
-public interface UserRepository extends CrudRepository<User,Long> {
-    User findByUsername(String username);
-}
